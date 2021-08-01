05e67b560153c798b34a1147d2e26e65dbd545c2
sena <43688294+katesenalee@users.noreply.github.com>
2021-08-01 14:37:45 +0800

    Initial Commit

---
+++ contacts/contactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactsApp.swift
+//  contacts
+//
+//  Created by Sena Lee on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct contactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

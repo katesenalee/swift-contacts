70441f0607f4241ed5e0c43e6028154a88680ebe
sena <43688294+katesenalee@users.noreply.github.com>
2021-08-08 15:53:13 +0800

    added contact deets

--- ContactDetailView.swift
+++ ContactDetailView.swift
@@ -10,13 +10,28 @@ import SwiftUI
 struct ContactDetailView: View {
     var contact: Contact
     var body: some View {
-        Text(contact.name)
-            .font(.largeTitle)
+        VStack {
+            Image(contact.name)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 175, height: 175)
+                .mask(Circle())
+                .padding(.top, 30)
+            Text(contact.name)
+                .font(.largeTitle)
+                
+            Text(contact.job)
+                .font(.title3)
+                .fontWeight(.medium)
+                .foregroundColor(.gray)
+                .padding(5)
+            Spacer()
+        }
     }
 }
 
 struct ContactDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ContactDetailView(contact: Contact(name: "barack obama", job: "not sure"))
+        ContactDetailView(contact: Contact(name: "holy zuck", job: "god"))
     }
 }
